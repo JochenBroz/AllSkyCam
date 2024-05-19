513629ad96998ef700d198f6a42c2b1ab75a0805
JochenBroz <54024001+JochenBroz@users.noreply.github.com>
2024-05-19 11:38:42 +0200

    Update getTemperature.py

--- getTemperature.py
+++ getTemperature.py
@@ -2,6 +2,14 @@
 import Adafruit_DHT
 import requests
 
+import RPi.GPIO as GPIO
+import sys
+
+GPIO.setmode(GPIO.BOARD)
+Signal_Pin = 16
+GPIO.setup(Signal_Pin, GPIO.OUT)
+print('Current heater state: ', GPIO.input(Signal_Pin))
+
 #Sensortyp und GPIO festlegen
 sensor = Adafruit_DHT.DHT11
 gpio = 4
@@ -9,8 +17,20 @@ gpio = 4
 # Daten auslesen
 url_string = 'http://influxDB.local:8086/write?db=signals'
 humidity, temperature = Adafruit_DHT.read_retry(sensor, gpio)
+
+if temperature <40:
+  GPIO.output(Signal_Pin, GPIO.HIGH)
+  print('New heater state: ', GPIO.input(Signal_Pin))
+
+if temperature >= 40:
+  GPIO.output(Signal_Pin, GPIO.LOW)
+  print('New heater state: ', GPIO.input(Signal_Pin))
+
+
+# Write Temperature to Influx
 temperature = 'temperature,location=AllSky value=%f'%temperature
 humidity = 'humidity,location=AllSky value=%f'%humidity
 
 rt = requests.post(url_string, data=temperature)
 rh = requests.post(url_string, data=humidity)
+
